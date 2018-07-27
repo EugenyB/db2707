d558c1d59050a7b42e60c5126eaa76aaba64634f
eugeny <eberkunsky@gmail.com>
2018-07-27 21:19:39 +0300

    CRUD operations

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,12 +18,43 @@ public class Main {
         properties.setProperty("characterEncoding", "utf8");
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mainacad1", properties);
-            List<Student> students = getStudents(connection);
-            students.forEach(System.out::println);
+            deleteStudent(connection);
+//            updateStudent(connection);
+//            Student student = new Student(0, "Vasya", "Lomachenko", 30);
+//            addStudent(connection, student);
+//            List<Student> students = getStudents(connection);
+//            students.forEach(System.out::println);
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void deleteStudent(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Student WHERE lastname = ?");
+        preparedStatement.setString(1, "Sergeev");
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+
+    private void updateStudent(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE Student SET name = ?, age = ? WHERE name = ?");
+        preparedStatement.setString(1, "Alexander");
+        preparedStatement.setInt(2, 19);
+        preparedStatement.setString(3, "Alex");
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+
+    private void addStudent(Connection connection, Student student) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "INSERT INTO Student (name, lastname, age) VALUES (?, ?, ?)"
+        );
+        preparedStatement.setString(1, student.getName());
+        preparedStatement.setString(2, student.getLastname());
+        preparedStatement.setInt(3, student.getAge());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
     }
 
     private List<Student> getStudents(Connection connection) throws SQLException {
